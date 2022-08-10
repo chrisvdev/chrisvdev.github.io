62fea0a005169d1a5be639d8ac72df961341c24e
Christian Villegas <chrisvill.2312@gmail.com>
2022-08-10 00:36:54 -0300

    advice of a site under construction...

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Site based on React under construction. </h1>
+        <p> By patient, this is going to be my landing page in a while... </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/christian-villegas-sanchez-66115097/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Christian Villegas
         </a>
       </header>
     </div>
