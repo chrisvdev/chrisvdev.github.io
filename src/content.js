274b17b4dbd7deef5b04eaf5bf2289656525b7bc
Christian Villegas <chrisvill.2312@gmail.com>
2022-09-04 03:55:47 -0300

    Modified content

--- src/content.js
+++ src/content.js
@@ -1,5 +1,7 @@
 import IMG1 from "./assets/demo.png";
 import ME from "./assets/about.jpg";
+import LANDING from "./assets/landing.jpg";
+import ALBT from "./assets/ALBT.png";
 
 const content = {
   alert: {
@@ -10,8 +12,7 @@ const content = {
   },
   about: {
     me: ME,
-    message:
-      `Graduated more than 10 years ago as a computer technician, I have always had the knowledge to enter software development, but the need pushed me to the branch of support for PCs, networks and servers. Instructed on my own in soft skills, to the point of, recently, having gone through the biggest non-verbal communication masterclass on the world, and as a photographer by hobby, which gave me my knowledge in graphic design, today, after having gone through the Bootcamp of "Soy Henry", I decide to make my career as a Full Stack Developer.`,
+    message: `Graduated more than 10 years ago as a computer technician, I have always had the knowledge to enter software development, but the need pushed me to the branch of support for PCs, networks and servers. Instructed on my own in soft skills, to the point of, recently, having gone through the biggest non-verbal communication masterclass on the world, and as a photographer by hobby, which gave me my knowledge in graphic design, today, after having gone through the Bootcamp of "Soy Henry", I decide to make my career as a Full Stack Developer.`,
   },
   experience: [
     {
@@ -110,66 +111,24 @@ const content = {
   ],
   portfolio: [
     {
-      image: IMG1,
-      title: "title",
+      image: LANDING,
+      title: "This web site!",
+      github:
+        "https://github.com/chrisvill2312/chrisvill2312.github.io/tree/main",
+    },
+    {
+      image: ALBT,
+      title: "ALBT (Arduino Lithium Battery Tester)",
       github: "https://github.com/chrisvill2312/arduino-li-battery-tester",
     },
     {
       image: IMG1,
       title: "title",
-      demo: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-    },
-    {
-      image: IMG1,
-      title: "title",
-      github: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-      demo: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-    },
-    {
-      image: IMG1,
-      title: "title",
-      github: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-      demo: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-    },
-    {
-      image: IMG1,
-      title: "title",
-      github: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-      demo: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-    },
-    {
-      image: IMG1,
-      title: "title",
-      github: "https://github.com/chrisvill2312/arduino-li-battery-tester",
-      demo: "https://github.com/chrisvill2312/arduino-li-battery-tester",
+      github: "https://github.com/chrisvill2312/",
+      demo: "https://github.com/chrisvill2312/",
     },
   ],
-  testimonials: [
-    {
-      avatar: "https://assets.soyhenry.com/icons/favicon-32x32.png",
-      name: "nombre",
-      review:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, iste veritatis maiores eligendi quas nam eaque vitae, alias ratione, soluta earum suscipit iure tenetur cum deleniti aliquid labore voluptatibus numquam.",
-    },
-    {
-      avatar: "https://assets.soyhenry.com/icons/favicon-32x32.png",
-      name: "nombre",
-      review:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, iste veritatis maiores eligendi quas nam eaque vitae, alias ratione, soluta earum suscipit iure tenetur cum deleniti aliquid labore voluptatibus numquam.",
-    },
-    {
-      avatar: "https://assets.soyhenry.com/icons/favicon-32x32.png",
-      name: "nombre",
-      review:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, iste veritatis maiores eligendi quas nam eaque vitae, alias ratione, soluta earum suscipit iure tenetur cum deleniti aliquid labore voluptatibus numquam.",
-    },
-    {
-      avatar: "https://assets.soyhenry.com/icons/favicon-32x32.png",
-      name: "nombre",
-      review:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, iste veritatis maiores eligendi quas nam eaque vitae, alias ratione, soluta earum suscipit iure tenetur cum deleniti aliquid labore voluptatibus numquam.",
-    },
-  ],
+  testimonials: undefined,
 };
 
 export default content;
