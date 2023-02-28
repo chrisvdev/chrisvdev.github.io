7006481c59314a9812d35f5dd413a2ab87373ea5
Christian Villegas <chrisvill.2312@gmail.com>
2023-02-27 22:28:10 -0300

    WIP: custom hook to get Github's repos data

--- src/hooks/github.js
+++ src/hooks/github.js
@@ -1,5 +1,14 @@
-import axios from ""
+import repo from "../repos"
 
 function useGithubRepos () {
+    const filtered = repo.map((e)=>{
+        const clon = structuredClone(e);
+        return {
+            name:clon.name,
+            description:clon.description,
+            updated:clon.updated_at,
+            tags:clon.topics,
+        }
+    })
     
 }
