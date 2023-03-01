e868fa702ffa74a19e1cd2fc518e6097093b007f
Christian Villegas <chrisvill.2312@gmail.com>
2023-03-01 04:18:13 -0300

    WIP: styled header, added contact

--- src/hooks/github.js
+++ src/hooks/github.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import repo from "../repos"
+import axios from "axios";
 
-function useGithubRepos () {
-    const filtered = repo.map((e)=>{
+function useGithubRepos(userName = "chrisvill2312") {
+    const [repos, setRepos] = useState([])
+    const processing = repo.map((e) => {
         const clon = structuredClone(e);
-        return {
-            name:clon.name,
-            description:clon.description,
-            updated:clon.updated_at,
-            tags:clon.topics,
-        }
+        return axios.get(clon.languages_url)
+            .then((response) => ({
+                langs: response.data, name: clon.name,
+                description: clon.description,
+                updated: clon.updated_at,
+                tags: clon.topics,
+            }))
     })
-    
+    try {
+        Promise.all(processing)
+            .then((repos) => (setRepos(repos)))
+    } catch (error) {
+        setRepos[{error:"error al cargar"}]
+    }
 }
