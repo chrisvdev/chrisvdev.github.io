3b113f1984b50c9ae077696edc9c5beb5c65ce34
Christian Villegas <chrisvill.2312@gmail.com>
2023-03-06 23:38:17 -0300

    WIP: Styling cards of the portfolio component

--- src/components/Pruebas.jsx
+++ src/components/Pruebas.jsx
@@ -13,20 +13,16 @@ export default function Pruebas() {
         .then((response) => {
           const processing = response.data.map((e) => {
             const clon = structuredClone(e);
-            return axios.get(clon.languages_url).then((response) => {
-              const previewTag = clon.topics.filter((tag) => tag.split(":")[0] === "preview")
-              const preview = previewTag.length>0 ? previewTag[0].split(":")[0] : "";
-              return {
-                langs: response.data,
-                name: clon.name,
-                description: clon.description,
-                updated: clon.updated_at,
-                tags: clon.topics,
-                deploy: clon.homepage,
-                github: clon.html_url,
-                preview: `https://res.cloudinary.com/dgg07ocbn/image/upload/v1/Landing/Projects/${}`,
-              };
-            });
+            return axios.get(clon.languages_url).then((response) => ({
+              langs: response.data,
+              name: clon.name,
+              description: clon.description,
+              updated: clon.updated_at,
+              tags: clon.topics,
+              deploy: clon.homepage,
+              github: clon.html_url,
+              preview: `https://res.cloudinary.com/dgg07ocbn/image/upload/w_384,h_216,c_scale/v1/Landing/Projects/${clon.name}`
+            }));
           });
           try {
             Promise.all(processing).then((repos) =>
@@ -43,15 +39,16 @@ export default function Pruebas() {
   }, [projects]);
   console.log(projects);
   return (
-    <section className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 max-w-7xl m-auto">
-      {projects.map(({ name, description, deploy, github }, index) => (
+    <section className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 max-w-7xl m-auto gap-4">
+      {projects.map(({ preview, name, description, deploy, github }, index) => (
         <article
           key={`project_${index}`}
-          className="flex flex-col max-w-xs justify-center items-center m-2 p-2"
+          className="flex flex-col max-w-xs max-h-fit justify-start items-center m-2 p-2 border border-amber-400 rounded shadow-lg  hover:border-slate-100 hover:scale-105 transition-all" //shadow-slate-600 hover:shadow-slate-50
         >
-          <h2 className="my-2 text-xl font-bold">{name}</h2>
-          <p className="mb-2">{description}</p>
-          <div className="w-full flex justify-around">
+          <img src={preview} alt={`${name}_preview`} className="aspect-video mb-4" />
+          <h2 className="mb-2 text-xl font-bold text-center">{name}</h2>
+          <p className="mb-4 text-center">{description}</p>
+          <div className="w-full flex justify-around mb-2">
             <LinkButton href={github}>GitHub</LinkButton>
             {deploy !== "" && <LinkButton href={deploy}>Visit</LinkButton>}
           </div>
