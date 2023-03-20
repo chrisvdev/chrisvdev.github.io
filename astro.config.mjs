8584a2ceba6339922c7780b19d588c68dfde77d7
Christian Villegas <chrisvill.2312@gmail.com>
2023-03-20 00:48:43 -0300

    FIX: Build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,16 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'entry.[hash].js',
+          chunkFileNames: 'chunks/chunk.[hash].js',
+          assetFileNames: 'assets/asset.[hash][extname]',
+        },
+      },
+    },
+  },
   integrations: [react(), tailwind()]
 });
